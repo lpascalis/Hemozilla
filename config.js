9916b2c123313be76f466e8ea84ed356191a6558
lpascalis <lpascalis@gmail.com>
2025-10-02 21:04:37 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-// config.js — HEMOZILLA (preconfigurato)
+// config.js — HEMOZILLA (preconfigurato con nuovi endpoint)
 window.API_DEFAULTS = {
   TAVI: 'https://script.google.com/macros/s/AKfycbxVYpirjmGNHYU_xkZ3d2pmbQo2at_D1rATUpJ9gE6DL1p1i___dotHegMF40q-S6fd/exec',
   TEER: 'https://script.google.com/macros/s/AKfycbx0fyOre2iqCVTTUYaq8fnhi6c3_j27qh7j5lCyyUZjbZyex7OZ4qzZ3hzNDrKgZWRx9w/exec',
