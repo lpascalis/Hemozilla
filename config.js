6cacd2b8a1795e5e6c012a6a9b32b9760b420a64
lpascalis <lpascalis@gmail.com>
2025-10-02 20:03:29 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,2 @@
 // URL default dei backend (Apps Script) per ciascun modulo
-window.API_DEFAULTS = {
-  TAVI: 'REPLACE_TAVI_URL',
-  TEER: 'REPLACE_TEER_URL',
-  LAAC: 'REPLACE_LAAC_URL'
-};
+window.API_DEFAULTS = { TAVI:'REPLACE_TAVI_URL', TEER:'REPLACE_TEER_URL', LAAC:'REPLACE_LAAC_URL' };
