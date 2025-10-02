77bb719cdcfb1128956265fc212949caad6b9169
lpascalis <lpascalis@gmail.com>
2025-10-02 20:55:23 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-// URL default dei backend (Apps Script) per ciascun modulo
+// config.js — HEMOZILLA (preconfigurato)
 window.API_DEFAULTS = {
-  TAVI: 'https://script.google.com/macros/s/AKfycbwDSij2nysD8YFFinEOOTUI7iIznZeMSwwTJrXsE7tB25lUC7og7RiC0fha9PlJxGfg/exec',
-  TEER: 'https://script.google.com/macros/s/AKfycbwQBBMt27NGk0KMXak0ihnl4_lBmjkF9LensgDbyo_t90no0PlQehCCwvwR_v3stRX1BA/exec',
-  LAAC: 'https://script.google.com/macros/s/AKfycbwK4OhhFKb8xUvr8sBp9jgPxuE5CModQrU6bgOmadkgWC2NmYtW65N9Mk5CroPtGZWu/exec'
+  TAVI: 'https://script.google.com/macros/s/AKfycbxVYpirjmGNHYU_xkZ3d2pmbQo2at_D1rATUpJ9gE6DL1p1i___dotHegMF40q-S6fd/exec',
+  TEER: 'https://script.google.com/macros/s/AKfycbx0fyOre2iqCVTTUYaq8fnhi6c3_j27qh7j5lCyyUZjbZyex7OZ4qzZ3hzNDrKgZWRx9w/exec',
+  LAAC: 'https://script.google.com/macros/s/AKfycbzSMlZGuBMAbcDpTBGLhnlSWJJhhPGETPoO8eBvw4b_Pn9sQIyFxHTrL27bG-3j3IU1/exec'
 };
